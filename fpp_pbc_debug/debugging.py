2111c0f25db041bac960029885047fef5394c0c4
Sean Koyama <koysean@gmail.com>
2021-04-14 15:31:10 -0500

    Debug script to show PBC not being respected by AMP when computing FPPrimes

---
+++ fpp_pbc_debug/debugging.py
@@ -0,0 +1,59 @@
+import ase
+import amp
+from amp.descriptor.gaussian import Gaussian, make_symmetry_functions
+from amp.utilities import get_hash
+import numpy as np
+
+cell = [6, 4, 4]
+
+xtl_centered = ase.Atoms('Si2',
+        positions = [[2,0,0], [4,0,0]],
+        cell = cell,
+        pbc = [True, True, True])
+
+xtl_edges =  ase.Atoms('Si2',
+        positions = [[1,0,0], [5,0,0]],
+        cell = cell,
+        pbc = [True, True, True])
+
+elements = ['Si']
+cutoff = 3
+symm_funcs = make_symmetry_functions(elements=elements, type='G2', etas=[1],
+        offsets=np.array([0, 0.5, 0.8])*cutoff)
+descriptor = Gaussian(Gs=symm_funcs, cutoff=cutoff)
+
+hash_centered = get_hash(xtl_centered)
+hash_edges = get_hash(xtl_edges)
+images = {hash_centered: xtl_centered, hash_edges: xtl_edges}
+descriptor.calculate_fingerprints(images, calculate_derivatives=True)
+
+fp_centered = descriptor.fingerprints[hash_centered]
+fp_edges = descriptor.fingerprints[hash_edges]
+
+fpp_centered = descriptor.fingerprintprimes[hash_centered]
+fpp_edges = descriptor.fingerprintprimes[hash_edges]
+
+print('FINGERPRINTS')
+print('Cutoff fully within cell.')
+print('PBC:', xtl_centered.pbc)
+for fp in fp_centered:
+    print(fp)
+print()
+
+print('Cutoff wraps around pbc.')
+print('PBC:', xtl_edges.pbc)
+for fp in fp_edges:
+    print(fp)
+print()
+
+print('FINGERPRINT PRIMES')
+print('Cutoff fully within cell.')
+print('PBC:', xtl_centered.pbc)
+for key, value in fpp_centered.items():
+    print(key, value)
+print()
+
+print('Cutoff wraps around pbc.')
+print('PBC:', xtl_edges.pbc)
+for key, value in fpp_edges.items():
+    print(key, value)
