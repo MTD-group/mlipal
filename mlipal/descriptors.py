48fb6bc583b7f586449d8695ebe617f7cf69d787
Sean Koyama <koysean@gmail.com>
2021-04-21 18:03:12 -0500

    added two_body_descriptors function, which generates two body descriptors. Currently only outputs the default.

--- mlipal/descriptors.py
+++ mlipal/descriptors.py
@@ -26,6 +26,30 @@ def calculate_fingerprints(atoms, descriptor):
     # NOTE: should this return just the fingerprints, without the hash?
     return {atoms_hash: descriptor.fingerprints[atoms_hash]}
 
+def two_body_gaussians(atoms, cutoff=3, num_etas=2, num_offsets=5, etas=None,
+        offsets=None):
+    ''' Generates two-body descriptors by spacing out gaussians and biasing
+    sampling towards the equilibrium position. '''
+
+    elements = set(atoms.get_chemical_symbols())
+    
+    cutoff = 3
+    # TODO: take num_etas and num_offsets and systematically generate etas and
+    # offsets arrays, only if etas and offsets are None. For now, resorts to
+    # default
+    if etas == None:
+        etas = [1,20]
+        # Generate etas
+    if offsets == None:
+        offsets = np.array([0, 0.3, 0.5, 0.68, 0.8]) * cutoff
+        # Generate offsets
+
+    symm_funcs = make_symmetry_functions(elements=elements, type='G2',
+            etas=etas, offsets=offsets)
+    descriptor = Gaussian(Gs=symm_funcs, cutoff=cutoff)
+
+    return descriptor
+
 def main():
     ''' This currently doesn't do anything but I'm keeping it as reference for
     our current descriptor set. '''
