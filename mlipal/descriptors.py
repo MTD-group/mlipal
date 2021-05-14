33d95347fe66bcc33775f24990b48cb7f37c71df
Sean Koyama <koysean@gmail.com>
2021-05-14 17:40:55 -0500

    modified default descriptor set

--- mlipal/descriptors.py
+++ mlipal/descriptors.py
@@ -2,7 +2,7 @@ import amp
 from amp.descriptor.gaussian import Gaussian, make_symmetry_functions
 import numpy as np
 
-def two_body_gaussians(atoms, num_etas=2, num_offsets=5, etas=None,
+def two_body_gaussians(atoms, cutoff=3, num_etas=2, num_offsets=5, etas=None,
         offsets=None):
     ''' Generates two-body symmetry functions by spacing out gaussians and
     biasing sampling towards the equilibrium position. '''
@@ -13,10 +13,10 @@ def two_body_gaussians(atoms, num_etas=2, num_offsets=5, etas=None,
     # offsets arrays, only if etas and offsets are None. For now, resorts to
     # default
     if etas == None:
-        etas = [1,20]
+        etas = [0.1, 1, 20]
         # Generate etas
     if offsets == None:
-        offsets = np.array([0, 0.3, 0.5, 0.68, 0.8]) * cutoff
+        offsets = np.array([0, 0.3, 0.5, 0.68, 0.72, 0.8, 0.9]) * cutoff
         # Generate offsets
 
     symm_funcs = make_symmetry_functions(elements=elements, type='G2',
@@ -36,10 +36,10 @@ def three_body_gaussians(atoms, num_etas=2, num_zetas=2, num_gammas=2,
     # offsets arrays, only if parameters (etas, zetas, gammas) are None. For
     # now, resorts to default
     if etas is None:
-        etas = [0.1,2]
+        etas = [0.01,1]
         # Generate etas
     if zetas is None:
-        zetas = [0.5, 1, 2, 4]
+        zetas = [0.01, 0.1, 0.5, 1]
         # Generate offsets
     if gammas is None:
         gammas = [-1, 1]
