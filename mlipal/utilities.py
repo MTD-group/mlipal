9ef8aefb4e4b18c31d6deb11e5ab4dc01452b7a2
Sean <koysean@gmail.com>
2021-05-25 14:50:44 -0500

    updated v1 of convert_db_to_df to be more streamlined and flexible.

--- mlipal/utilities.py
+++ mlipal/utilities.py
@@ -14,4 +14,34 @@ def convert_db_to_df(db, descriptor=None, columns=None):
         the dataframe will include columns for energy, forces, and all extra
         columns in the db.'''
 
+    count = 0
+    for row in db.select():
+        descriptor.calculate_fingerprints({row.hash: row.toatoms()})
 
+        hashes = np.asarray(row.hash).reshape(1,)
+
+        structure_type = np.asarray(row.type).reshape(1,)
+
+        energy = row.energy / row.natoms
+        energy = np.array(energy).reshape(1,)
+
+        forces = row.forces
+
+        for i in range(row.natoms):
+            atoms_fp = descriptor.fingerprints[row.hash][i]
+            element = np.asarray(atoms_fp[0]).reshape(1,)
+            feature = np.array(atoms_fp[1])
+            force = forces[i]
+            atom_info = np.concatenate([hashes, structure_type, energy, force,
+                element, feature])
+            atom_info = np.reshape(atom_info, (1, atom_info.shape[0]))
+            if count==0:
+                atom_data = atom_info
+                count = 1
+            else:
+                atom_data = np.append(atom_data, atom_info, axis=0)     
+
+    df = pd.DataFrame(atom_data)  
+    df = df.rename(columns={0: "hash", 1:"type", 2: "element", 3: "energy", 4: "force0", 5: "force1", 6: "force2"})             
+
+    return df
